2f66a19f762a1147c2c38115a467b6f39e464e27
ndry0611 <andryss0611@gmail.com>
2023-12-21 11:04:06 +0700

    install dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../theme";
 
 export const metadata = {
-  title: "Mantine Next.js template",
-  description: "I am using Mantine with Next.js!",
+  title: "Toko Ria Sigli",
+  description: "Autoparts Shop",
 };
 
 export default function RootLayout({ children }: { children: any }) {
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en">
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/logo.svg" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
