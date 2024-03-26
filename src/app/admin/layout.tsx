46f424ec1a5f0230af16c2531387b37bd6e2c97f
ndry0611 <andryss0611@gmail.com>
2024-03-27 00:37:54 +0700

    initiate admin page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,19 +1,17 @@
 import "@mantine/core/styles.css";
 import React from "react";
-import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { Flex, MantineProvider } from "@mantine/core";
 import { NavbarSimple } from "../../component/common/navigation";
-import { Content } from "../../component/common/content";
-
-export const metadata = {
-  title: "Toko Ria Sigli",
-  description: "Autoparts Shop",
-};
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <MantineProvider>
-      <NavbarSimple />
-      <Content>{children}</Content>
-    </MantineProvider>
+    <>
+      <Flex>
+        <NavbarSimple />
+        <Flex w={"100%"} mah={"100vh"} style={{ overflow: "auto" }}>
+          {children}
+        </Flex>
+      </Flex>
+    </>
   );
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,42 @@
+"use client";
+import { Flex, Grid } from "@mantine/core";
+import { useEffect } from "react";
+import { getTokenStorage } from "../../hooks/use-token";
+import React from "react";
+import { callApi } from "../../utils/api";
+
+export interface Category {
+  id: number;
+  name: string;
+  description: string;
+  file_name: string | null;
+}
+
 export default function HomePage() {
+  const [catalog, setCatalog] = React.useState<Category[]>([]);
+  useEffect(() => {
+    try {
+      const getCatalog = async () => {
+        const result = await callApi({ url: "/category" });
+        const data = await result.json();
+        setCatalog(data);
+      };
+      getCatalog();
+    } catch (error) {
+      //Sent user/admin to 500 page (?)
+    }
+  }, []);
   return (
-    <>
-      <p>gantenk</p>
-    </>
+    <Flex w={"100%"} justify="center">
+      <Grid>
+        {catalog.map((item) => {
+          return (
+            <Grid.Col span={4} key={item.id}>
+              {item.name}
+            </Grid.Col>
+          );
+        })}
+      </Grid>
+    </Flex>
   );
 }
