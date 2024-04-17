82c7910949686fd2b57720b5998c4db88617aaca
ndry0611 <andryss0611@gmail.com>
2024-04-17 12:23:04 +0700

    update table style

--- src/modules/admin/component/table-list.tsx
+++ src/modules/admin/component/table-list.tsx
@@ -2,17 +2,19 @@ import { Table, TableProps } from "@mantine/core";
 
 export default function TableList(props: TableProps) {
   return (
-    <Table
-      {...props}
-      styles={{
-        thead: {
-          backgroundColor: "#FF852D",
-          color: "white",
-        },
-        th: {
-          fontWeight: "bold",
-        },
-      }}
-    />
+    <Table.ScrollContainer minWidth={1000}>
+      <Table
+        {...props}
+        styles={{
+          thead: {
+            backgroundColor: "#FF852D",
+            color: "white",
+          },
+          th: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </Table.ScrollContainer>
   );
 }
