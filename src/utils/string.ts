172a85a0039b4ef888789715f8b68a64da254a79
ndry0611 <andryss0611@gmail.com>
2024-04-17 12:05:06 +0700

    add date formatter

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,7 +1,18 @@
+import { format } from "date-fns";
+
 export function stringToMoney(input: string | number): string {
   const value = typeof input === "number" ? input : parseFloat(input);
   if (isNaN(value)) return "";
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
   }).format(value);
+}
+
+export function formatDate(date: string) {
+  try {
+    const result = new Date(date);
+    return format(result, "dd/MMMM/yyyy, HH:mm");
+  } catch (error: any) {
+    return error.message;
+  }
 }
