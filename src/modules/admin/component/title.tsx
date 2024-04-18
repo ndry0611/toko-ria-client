f360ec80f95c9b846ef664740bbac405b27d96d4
ndry0611 <andryss0611@gmail.com>
2024-04-18 22:10:39 +0700

    remove unused code

--- src/modules/admin/component/title.tsx
+++ src/modules/admin/component/title.tsx
@@ -1,7 +1,6 @@
 import { Text, TextProps } from "@mantine/core";
 import React from "react";
 
-interface TitleTextProps extends TextProps {}
 export default function TitleText({ children }: { children: React.ReactNode }) {
   return (
     <Text fz={26} fw={500}>
