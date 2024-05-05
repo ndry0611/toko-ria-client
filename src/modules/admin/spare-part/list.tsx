041585747cd1a04e2fb992ef377cf053d9289b3e
ndry0611 <andryss0611@gmail.com>
2024-05-06 00:20:52 +0700

    try to add filter

--- src/modules/admin/spare-part/list.tsx
+++ src/modules/admin/spare-part/list.tsx
@@ -1,20 +1,46 @@
-import { Flex, Space } from "@mantine/core";
+import { Flex, SimpleGrid, Space } from "@mantine/core";
 import { useGetSpareParts } from "../../../api-hooks/sparePart-api";
 import ItemPaper from "./component/item-paper";
 import TitleText from "../component/title";
 import LoaderView from "../component/loader-view";
 import CreateButton from "../component/create-button";
+import FindButton from "../component/find-button";
+import { useForm } from "react-hook-form";
+import Form from "../../../component/form";
+import { useGetCategories } from "../../../api-hooks/category-api";
+import Input from "../../../component/input";
 
 export default function SparePartList() {
+
+  //Temporary
+  const methods = useForm({
+    defaultValues: {
+      id_category: 0
+    }
+  })
+
+  const queryCategory = useGetCategories();
+  const { data: categoryData = [] } = queryCategory;
+  const categories = categoryData.map((obj) => ({
+    value: obj.id.toString(),
+    label: obj.name,
+  }));
+
   const query = useGetSpareParts();
   const { data = [] } = query;
   return (
     <>
       <TitleText>Barang</TitleText>
       <Space h={"sm"} />
-      <Flex justify={"right"}>
-        <CreateButton />
-      </Flex>
+      <Form methods={methods} onSubmit={() => {}}>
+        <SimpleGrid cols={3}>
+          <Input type="select" name="id_category" data={categories} />
+        </SimpleGrid>
+        <Flex justify={"right"} gap={15} m={5}>
+          <FindButton qKey={["get-spare-parts"]} />
+          <CreateButton />
+        </Flex>
+      </Form>
       <Flex direction={"column"} gap={15}>
         <LoaderView query={query}>
           {(data) =>
