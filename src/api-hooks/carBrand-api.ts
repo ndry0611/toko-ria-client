54be51dc5f7a8597a4e23117c6bd43da6cbfbad4
ndry0611 <andryss0611@gmail.com>
2024-05-06 22:01:33 +0700

    feat: filter using id_category and id_car_brand

---
+++ src/api-hooks/carBrand-api.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { callApi } from "../utils/api";
+
+// Will moved to car-brand/component/type.ts
+export interface CarBrandModel {
+  id: number;
+  name: string;
+  manufacture: string;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export function useGetCarBrand() {
+  return useQuery({
+    queryKey: ["get-car-brands"],
+    queryFn: async () =>
+      await callApi<CarBrandModel[]>({
+        url: "/car-brand",
+        method: "GET",
+      }),
+  });
+}
--- src/modules/admin/spare-part/list.tsx
+++ src/modules/admin/spare-part/list.tsx
@@ -5,19 +5,15 @@ import TitleText from "../component/title";
 import LoaderView from "../component/loader-view";
 import CreateButton from "../component/create-button";
 import FindButton from "../component/find-button";
-import { useForm } from "react-hook-form";
+import { useForm, useFormContext, useWatch } from "react-hook-form";
 import Form from "../../../component/form";
 import { useGetCategories } from "../../../api-hooks/category-api";
 import Input from "../../../component/input";
+import { useGetCarBrand } from "../../../api-hooks/carBrand-api";
 
 export default function SparePartList() {
-
-  //Temporary
-  const methods = useForm({
-    defaultValues: {
-      id_category: 0
-    }
-  })
+  const methods = useForm();
+  const { getValues } = methods;
 
   const queryCategory = useGetCategories();
   const { data: categoryData = [] } = queryCategory;
@@ -26,15 +22,28 @@ export default function SparePartList() {
     label: obj.name,
   }));
 
-  const query = useGetSpareParts();
-  const { data = [] } = query;
+  const queryCarBrand = useGetCarBrand();
+  const {data: carBrandData = []} = queryCarBrand;
+  const carBrands = carBrandData.map((obj) => ({
+    value: obj.id.toString(),
+    label: obj.name
+  }));
+
+  const params = {
+    id_category: getValues("id_category"),
+    id_car_brand: getValues("id_car_brand")
+  };
+
+  const querySparePart = useGetSpareParts(params);
+  const { data: sparePartData = [] } = querySparePart;
   return (
     <>
       <TitleText>Barang</TitleText>
       <Space h={"sm"} />
       <Form methods={methods} onSubmit={() => {}}>
         <SimpleGrid cols={3}>
-          <Input type="select" name="id_category" data={categories} />
+          <Input type="select" name="id_category" label="Kategori" data={categories} />
+          <Input type="select" name="id_car_brand" label="Merk Mobil" data={carBrands} />
         </SimpleGrid>
         <Flex justify={"right"} gap={15} m={5}>
           <FindButton qKey={["get-spare-parts"]} />
@@ -42,9 +51,9 @@ export default function SparePartList() {
         </Flex>
       </Form>
       <Flex direction={"column"} gap={15}>
-        <LoaderView query={query}>
-          {(data) =>
-            data.map((item) => {
+        <LoaderView query={querySparePart}>
+          {(sparePartData) =>
+            sparePartData.map((item) => {
               return (
                 <ItemPaper
                   key={item.id}
