0427f0b302c55abe25d2978deac65d62f0c7d605
ndry0611 <andryss0611@gmail.com>
2024-05-09 22:39:57 +0700

    feat: add theme

--- theme.ts
+++ theme.ts
@@ -1,7 +1,23 @@
 "use client";
 
-import { createTheme } from "@mantine/core";
+import { MantineColorsTuple, createTheme } from "@mantine/core";
 import { themeToVars } from "@mantine/vanilla-extract";
 
-export const theme = createTheme({});
+const myColor: MantineColorsTuple = [
+  '#fff1e1',
+  '#ffe2cb',
+  '#ffc49a',
+  '#ff852d',
+  '#ff8a36',
+  '#ff7918',
+  '#ff7006',
+  '#e45e00',
+  '#cb5200',
+  '#b14500'
+];
+
+export const theme = createTheme({
+  colors:{myColor},
+  primaryColor: 'myColor'
+});
 export const vars = themeToVars(theme);
