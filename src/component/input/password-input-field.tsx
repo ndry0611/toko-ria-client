15f28d1cf3e57cf0c7d804ee9be9544b7d26db4f
ndry0611 <andryss0611@gmail.com>
2024-05-11 21:32:50 +0700

    feat: add password input

---
+++ src/component/input/password-input-field.tsx
@@ -0,0 +1,42 @@
+import { PasswordInput, PasswordInputProps } from "@mantine/core";
+import { useFormState } from "../form";
+import { useController } from "react-hook-form";
+
+export interface PasswordInputFieldProps extends PasswordInputProps {
+  type: "password";
+  name: string;
+  onAfterChange?: (value: string) => void;
+}
+
+export default function PasswordInputField(props: PasswordInputFieldProps) {
+  const {
+    name,
+    type,
+    disabled,
+    readOnly,
+    rightSection,
+    onAfterChange,
+    ...rest
+  } = props;
+  const formState = useFormState();
+  const { field, fieldState } = useController({
+    name,
+  });
+
+  const _disabled = disabled || readOnly || formState.disabled;
+  const error = fieldState.error?.message;
+
+  return (
+    <PasswordInput
+      {...rest}
+      {...field}
+      disabled={_disabled}
+      error={error}
+      inputWrapperOrder={["label", "input", "description", "error"]}
+      onChange={(val) => {
+        field.onChange(val.currentTarget.value);
+        onAfterChange?.(val.currentTarget.value);
+      }}
+    />
+  );
+}
